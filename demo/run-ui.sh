6be74bae5fbfe303c4a501a923519686f17acfb0
dzhuribeda <dzhuribeda@cybervisiontech.com>
2016-03-02 18:25:33 +0200

    classpath generalize

--- demo/run-ui.sh
+++ demo/run-ui.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd rest-mock
-java -Xdebug -agentlib:jdwp=transport=dt_socket,address=9999,server=y,suspend=n -cp "/opt/mapr/conf:/opt/mapr/lib/mapr-streams-5.1.0-mapr-SNAPSHOT.jar:/opt/mapr/lib/kafka-clients-0.9.0.0-mapr-SNAPSHOT.jar:/opt/mapr/lib/maprfs-5.1.0-mapr-SNAPSHOT.jar:target/telemetryui.jar" com.mapr.samples.racingseries.Main
+java -Xdebug -agentlib:jdwp=transport=dt_socket,address=9999,server=y,suspend=n -cp "/opt/mapr/conf:/opt/mapr/lib/*:target/telemetryui.jar" com.mapr.samples.racingseries.Main
